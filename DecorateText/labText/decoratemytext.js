bc8c858c3f89de4cf7dc9674816c9c51eadc048f
uulen0201 <79428070+uulen0201@users.noreply.github.com>
2021-05-26 01:00:44 -0500

    decorate text

--- DecorateText/labText/decoratemytext.js
+++ DecorateText/labText/decoratemytext.js
@@ -1,39 +1,57 @@
+var timer = null;
+function onClickTimer(){
+    if(timer == null)
+        timer = setInterval(bigger,500);    
+    else{
+        clearInterval(timer);
+        timer = null;
+    }
+}
+function bigger() {
+    var txt = document.getElementById("text");
+    if(txt.style.fontSize != "")
+        var fsize = parseInt(txt.style.fontSize);
+    else
+        var fsize = 12;
+    fsize += 2;
+    txt.style.fontSize = fsize + "pt";    
+}
+function bling(){
+    var txt = document.getElementById("text");
+    var chk = document.getElementById("bling");
+    if(chk.checked === true){
+        txt.style.fontWeight = "bold";
+        txt.style.color = "green";
+        txt.style.textDecorationLine="underline";
+        document.body.style.backgroundImage = "URL('images/silverware.jpg')";
+    }
+    else{
+        txt.style.fontWeight = "normal";
+        txt.style.color = "black";
+        txt.style.textDecoration="none";
+    }
+}
+function pigLatin(){
+    var txt = document.getElementById("text");
+    var value = txt.innerHTML;
+    if(value.charAt(0) == 'S') 
+        value = '-ay' + value.substring(1,value.length-1) + value.substring(0,1);
+    else
+        value = value + '-ay';
 
-(function() {
-	"use strict";
-	var timer = null;
+    txt.innerHTML = value;
+}
 
-	function biggerDecorations() {
-		if (timer == null) {
-		    timer = setInterval(biggerFont, 500);
-		} else {
-		    clearInterval(timer);
-			timer = null;
-		}
-	}
+function malkovich(){
+    var txt = document.getElementById("text");
+    var value = txt.innerHTML;
+    var strArray = value.split(' ');
+    for(var i=0 ; i< strArray.length; i++) {
+        if(strArray[i].length >= 5){
+            strArray[i] = "Malkovich";
+        }
+    }
+    value = strArray.join(' ');
 
-	function biggerFont() {
-		var textArea = document.getElementById("text-decorations");
-		var size = parseInt(textArea.style.fontSize) ? parseInt(textArea.style.fontSize) : 12;
-		size += 2;
-  		textArea.style.fontSize =  size + "pt";
-	}
-
-	function changeFontWeight() {
-		var textArea = document.getElementById("text-decorations");
-		var body = document.body;
-
-		if (!textArea.className || textArea.className === "normalFont") {
-			textArea.className = "boldFont greenColor";
-			body.className = "background";
-		} else if (textArea.className === "boldFont greenColor") {
-		    textArea.className = "normalFont";
-		    body.className = "";
-		}
-	}
-
-	window.onload = function() {
-		document.getElementById("bigger-decorations").onclick = biggerDecorations;
-		document.getElementById("bling").onchange = changeFontWeight;
-	}
-})();
+    txt.innerHTML = value;
+}

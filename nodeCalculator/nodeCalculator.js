66128913e1f32c3c176cca899586c74ce14bd832
uulen0201 <79428070+uulen0201@users.noreply.github.com>
2021-06-07 17:02:19 -0500

    Update nodeCalculator.js

--- nodeCalculator/nodeCalculator.js
+++ nodeCalculator/nodeCalculator.js
@@ -1,13 +1,13 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-var calcMod = require('./nodeCalculatorModule.js');
+var calcMod = require('nodeCalculatorModule.js');
 
 http.createServer(function(req, res){
     var q = url.parse(req.url, true);
     var filename = "." + q.pathname;
 
-    if (q.pathname=="/nodeCalculator.js")
+    if (q.pathname=="nodeCalculator.js")
         calcMod.calc(req,res,q.query)
     else
         fs.readFile(filename, function(err, data) {

7716dc588a427862815a7050a807beba755138e5
uulen0201 <79428070+uulen0201@users.noreply.github.com>
2021-06-09 04:15:12 -0500

    Update word.js

--- DictionaryProject/word.js
+++ DictionaryProject/word.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: '127.0.0.1',
     port: '3306',
-    user: 'Uulen',
+    user: 'uulen',
     password: 'root',
     database: 'entries'
 });

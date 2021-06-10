5e7b52d9521df184e561aeaa91c41e5900687239
uulen0201 <79428070+uulen0201@users.noreply.github.com>
2021-06-10 08:51:32 -0500

    Update nodeCalculator.js

--- nodeCalculator/nodeCalculator.js
+++ nodeCalculator/nodeCalculator.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-var calcMod = require('nodeCalculatorModule.js');
+var calcMod = require('./nodeCalculatorModule.js');
 
 http.createServer(function(req, res){
     var q = url.parse(req.url, true);
